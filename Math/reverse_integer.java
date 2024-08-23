98e03b7e9f4d482b0c3c53f07a77b20604ea83d7
Goutham Kumar S <113292942+GouthamKumar025@users.noreply.github.com>
2024-08-23 08:35:26 +0530

    Update reverse_integer.java

--- Math/reverse_integer.java
+++ Math/reverse_integer.java
@@ -1,14 +1,17 @@
 class Solution {
     public int reverse(int x) {
         int rev = 0;
-        int sign = (x < 0)? -1: 1;
-        x = Math.abs(x);
-        while(x>0){
+
+        while (x != 0) {
             int rem = x % 10;
+            // by multiplying 10 we can know that overflow
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
             rev = rev * 10 + rem;
-            x/=10;
+            x /= 10;
         }
-        rev *= sign;
+
         return rev;
     }
 }
