8c69d455ec10f5b9a0925782e4c3834a49f8b4f3
Goutham Kumar S <113292942+GouthamKumar025@users.noreply.github.com>
2024-10-14 10:17:34 +0530

    14-10-24

--- Two Pointers/two_sum.java
+++ Two Pointers/two_sum.java
@@ -1,13 +1,52 @@
+// T C - O(N)
+
 class Solution {
     boolean twoSum(int arr[], int target) {
-        // code here
+        // using hashmap - optimized
+        HashMap<Integer,Integer>map = new HashMap<>();
+        
         for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i] + arr[j] == target){
-                    return true;
-                }
+            int complement = target - arr[i];
+            if(map.containsKey(complement)){
+                return true;
             }
+            map.put(arr[i],i);
         }
         return false;
     }
+}
+
+
+  // two - pointer approach - O(NLogN)
+//   int i = 0, j = n-1;
+//   while(i< j){
+//       int sum = arr[i] + arr[j];
+//       if(sum == target){
+//           return true;
+//       }
+//       else if(sum < target){
+//           i++;
+//       }
+//       else{
+//           j --;
+//       }
+//   }
+// return false;
+  
+
+
+
+
+    // normal loops - O(N^2)
+    // boolean twoSum(int arr[], int target) {
+    //     // code here
+    //     for(int i=0;i<arr.length;i++){
+    //         for(int j=i+1;j<arr.length;j++){
+    //             if(arr[i] + arr[j] == target){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
 }
