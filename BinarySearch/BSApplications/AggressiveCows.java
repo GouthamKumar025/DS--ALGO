c8066a546a7b3673cffef2cb49a3419881ab342f
Goutham Kumar S <113292942+GouthamKumar025@users.noreply.github.com>
2024-10-26 19:38:38 +0530

    26-10-24

---
+++ BinarySearch/BSApplications/AggressiveCows.java
@@ -0,0 +1,35 @@
+class Solution {
+    public static int solve(int[] stalls, int k) {
+        int n = stalls.length;
+        Arrays.sort(stalls);
+        int low = 1;
+        int high = stalls[n-1] - stalls[0];
+        int ans=  0;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(canWePlace(stalls,k,mid)){
+                ans = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+    
+    public static boolean canWePlace(int[]stalls, int cows, int mid){
+        int cntCows = 1;
+        int lastCow = stalls[0];
+        for(int i = 1; i < stalls.length; i++){
+            if(stalls[i] - lastCow >= mid){
+                cntCows++;
+                lastCow = stalls[i];
+            }
+            if(cntCows >= cows){
+                return true;
+            }
+        }
+        return false;
+    }
+}
